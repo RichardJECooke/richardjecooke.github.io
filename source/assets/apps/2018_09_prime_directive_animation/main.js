c1dbf49c642155cc092f1dbe74a32ddf9a5f69ff
Richard JE Cooke <richard.j.e.cooke@gmail.com>
2018-09-29 09:19:57 +0200

    work on animation

--- source/assets/apps/2018_09_prime_directive_animation/main.js
+++ source/assets/apps/2018_09_prime_directive_animation/main.js
@@ -1,3 +1,7 @@
+//refs
+//sketchfab: https://sketchfab.com/search?features=downloadable&q=tree&sort_by=-pertinence&type=models
+//https://sketchfab.com/models/76d4b22511aa41829da542b01ade8698#download
+
 //constants
 const songFilename = '/assets/apps/2018_09_prime_directive_animation/2018-09 - Richard JE Cooke - Prime Directive - Version 6.mp3';
 const volume = 0.5;
@@ -12,21 +16,18 @@ const listener = new THREE.AudioListener();
 const song = new THREE.Audio(listener);
 const audioLoader = new THREE.AudioLoader();
 //shapes
-let _bass1 = null;
-let _bass2 = null;
-let _sphere = null;
+let ground = null;
+let bass1 = null;
+let bass2 = null;
+let sphere = null;
 //logic
-let isPlaying = false;
+let isPlaying = true; //TODO set to false
 
 window.onload = start;
 
 function start()
 {
 	createScene();
-	let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	let material = new THREE.MeshBasicMaterial( { 'color': '#841' } );
-	_bass1 = new THREE.Mesh( geometry, material);
-	scene.add(_bass1);
 	camera.position.z = 10;
 	animate();
 }
@@ -36,7 +37,15 @@ function createScene()
 	renderer.setSize(canvasWidth, canvasHeight);
 	document.body.appendChild(renderer.domElement);
 	loadAudio();
+	addGround();
+}
 
+function addGround()
+{
+	let geometry = new THREE.BoxGeometry(100, 1, 100);
+	let material = new THREE.MeshBasicMaterial( { 'color': '#292' } );
+	ground = new THREE.Mesh(geometry, material);
+	scene.add(ground);
 }
 
 function loadAudio()
@@ -53,15 +62,14 @@ function animate()
 {
 	if (!isPlaying) return;
 	requestAnimationFrame(animate);
-	_bass1.rotation.x += 0.01;
-	_bass1.rotation.y += 0.02;
-	_bass1.rotation.z += 0.03;
+	// bass1.rotation.x += 0.01;
+	// bass1.rotation.y += 0.02;
+	// bass1.rotation.z += 0.03;
 	renderer.render(scene, camera);
 }
 
 function userClickedPlay()
 {
-	console.log('play');
 	isPlaying = true;
 	song.play();
 	animate();
@@ -69,7 +77,6 @@ function userClickedPlay()
 
 function userClickedPause()
 {
-	console.log('pause');
 	isPlaying = false;
 	song.pause();
 }
